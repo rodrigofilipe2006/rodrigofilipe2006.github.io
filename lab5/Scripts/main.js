418864f294bcfac72dba307559db47d6ff6dbbf9
rodrigofilipe <a22402770@alunos.ulht.pt>
2025-10-13 11:41:28 +0000

    input arco iris terminado e a meio do eventos 2.0

--- lab5/Scripts/main.js
+++ lab5/Scripts/main.js
@@ -11,7 +11,7 @@ const butaoGreen = buttons[1];
 const butaoBlue = buttons[2];
 
 //3
-
+const colorInput = document.querySelector('#textoRGB input')
 
 //4
 const textoFundo = document.querySelector('#corFundo input');
@@ -22,7 +22,10 @@ const botaoConta = document.querySelector('#conta button');
 const numeroConta = document.querySelector('#conta p:last-child');
 
 //6
-
+const inputNome = document.querySelector('#nomeIdade input:first-child')
+const inputIdade = document.querySelector('#nomeIdade input:last-child')
+const botaoForm = document.querySelector('#nomeIdade button')
+const userPrint = document.querySelector('#nomeIdade p:last-child')
 
 //7
 
@@ -47,7 +50,13 @@ mudaCorTexto = (cor) => {
 }
 
 //3
+caixaRGB = () => {
+    redRandom = Math.floor(Math.random() * 256);
+    greenRandom = Math.floor(Math.random() * 256);
+    blueRandom = Math.floor(Math.random() * 256);
 
+    colorInput.style.backgroundColor = `rgb(${redRandom},${greenRandom},${blueRandom})`
+}
 
 //4
 
@@ -62,7 +71,9 @@ contador = () => {
 }
 
 //6
-
+idadeNome = (nome , idade) => {
+    userPrint.innerHTML = 'Olá, o ' + nome + ' tem ' + idade
+}
 
 //7
 
@@ -71,25 +82,25 @@ contador = () => {
 //Event listener
 
 //1
-hoverText.onmouseover = () => passaPorCima(true)
-hoverText.onmouseout = () => passaPorCima(false)
+hoverText.onmouseover = () => passaPorCima(true);
+hoverText.onmouseout = () => passaPorCima(false);
 
 //2
-butaoRed.onclick = () => mudaCorTexto('red')
-butaoGreen.onclick = () => mudaCorTexto('green')
-butaoBlue.onclick = () => mudaCorTexto('blue')
+butaoRed.onclick = () => mudaCorTexto('red');
+butaoGreen.onclick = () => mudaCorTexto('green');
+butaoBlue.onclick = () => mudaCorTexto('blue');
 
 //3
-
+colorInput.onkeydown = () => caixaRGB()
 
 //4
-botaoFundo.onclick = () => mudaCorFundo(textoFundo.value)
+botaoFundo.onclick = () => mudaCorFundo(textoFundo.value);
 
 //5
-botaoConta.onclick = () => contador()
+botaoConta.onclick = () => contador();
 
 //6
-
+botaoForm.onclick = () => idadeNome(inputNome , inputIdade)
 
 //7
 
